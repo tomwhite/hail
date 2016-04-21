2fea5eea5f7ac536a5dcc5e3a86ca3ea0998be53
laurent <lfran@broadinstitute.org>
2016-04-21 14:04:56 -0400

    * Switched from GenotypeType => Genotype -- So much cleaner!

---
+++ src/main/scala/org/broadinstitute/hail/methods/GeneBurden.scala
@@ -0,0 +1,97 @@
+package org.broadinstitute.hail.methods
+
+import org.apache.spark.rdd.RDD
+import org.broadinstitute.hail.methods.PhasedGenotypeType.PhasedGenotypeType
+import org.broadinstitute.hail.utils.SparseVariantSampleMatrix
+import org.broadinstitute.hail.variant.{Genotype, GenotypeType}
+import org.broadinstitute.hail.variant.GenotypeType._
+import org.apache.commons.math3.stat.inference
+import org.apache.commons.math3.stat.inference.{AlternativeHypothesis, BinomialTest}
+
+import scala.collection.mutable
+
+//Super-ugly!
+object PhasedGenotypeType extends  Enumeration{
+  type PhasedGenotypeType = Value
+  val HomRef = Value(0)
+  val Het = Value(1)
+  val HomVar = Value(2)
+  val NoCall = Value(-1)
+  val CompHet = Value(3)
+
+}
+
+//class GeneBurden(val caseCounts : mutable.HashMap[PhasedGenotypeType,Int],val controlCounts : mutable.HashMap[PhasedGenotypeType,Int], val cumulAF : Double)  {
+class GeneBurden(val cases: SparseVariantSampleMatrix, val controls: SparseVariantSampleMatrix){
+
+  //Compute counts
+  val casesCounts = getCounts(cases, controls)
+  val controlsCounts = getCounts(controls, controls,2)
+  val cumulAF = controls.cumulativeAF //TODO Correct for in-phase hets
+
+  //Compute p-values
+  var pValRec = computeRecessiveEnrichment()
+  var pValDom = 1
+
+  //Stores variants / genotype info for all significantly enriched genes
+  val casesVariants = if(pValRec < 5e-5 || pValDom < 5e-5) cases.variants else mutable.Map[String, mutable.Map[String,Genotype]]()
+  val controlsVariants = if(pValRec < 5e-5 || pValDom < 5e-5) controls.variants else mutable.Map[String, mutable.Map[String,Genotype]]()
+
+  /** Public Functions **/
+  override def toString(): String ={
+    return ""
+  }
+
+  /** Private Functions **/
+  private def computeRecessiveEnrichment(): Double = {
+    val rate = cumulAF * cumulAF
+    new BinomialTest().binomialTest(cases.nSamples, casesCounts(PhasedGenotypeType.HomVar) + casesCounts(PhasedGenotypeType.CompHet), rate, AlternativeHypothesis.GREATER_THAN)
+  }
+
+  private def isCompoundHet(variantIDs: List[String], phasingData: SparseVariantSampleMatrix, minPhasingSamples: Int = 1) : Boolean = {
+
+    val phasingVariants = phasingData.variants
+
+    //Loop over all pairs of variants (ordered lexicographically)
+    (for( i<-variantIDs; j <- variantIDs; if(i < j && phasingVariants.contains(i) && phasingVariants.contains(j))) yield{
+      //For each pair of variants, count how many samples carry both
+      (phasingVariants(i).filter({case (k,v) => v == GenotypeType.Het || v == GenotypeType.HomVar}).keySet.intersect(
+        phasingVariants(j).filter({case (k,v) => v == GenotypeType.Het || v == GenotypeType.HomVar}).keySet).size)
+
+    }).max > minPhasingSamples
+  }
+
+  private def getCounts(vs: SparseVariantSampleMatrix, phasingData: SparseVariantSampleMatrix, minPhasingSamples: Int = 1) : mutable.HashMap[PhasedGenotypeType,Int] = {
+    val res = new mutable.HashMap[PhasedGenotypeType,Int]()
+
+    vs.samples.foreach({ case (sampleID, variants) =>
+      //Goes from Variant -> GT to GT -> Variants -- TODO: Unless Variant -> GT used elsewhere, change the representation in SVSM
+      val byGT = variants.foldRight(Map[GenotypeType, List[String]]())({
+        case (variant, acc) => acc updated(variant._2.gtType, variant._1 :: acc.getOrElse(variant._2.gtType, List[String]()))
+      })
+
+      //Count genotypes in order of deleteriousness
+      if (byGT.contains(GenotypeType.HomVar)) {
+        res.update(PhasedGenotypeType.HomVar, res.getOrElse(PhasedGenotypeType.HomVar, 0) + 1)
+      }
+      else if (byGT.contains(GenotypeType.Het)) {
+        if (byGT.get(GenotypeType.Het).size > 2 && isCompoundHet(byGT(GenotypeType.Het), phasingData, minPhasingSamples)) {
+          res.update(PhasedGenotypeType.Het, res.getOrElse(PhasedGenotypeType.CompHet, 0) + 1)
+        }
+        else {
+          res.update(PhasedGenotypeType.Het, res.getOrElse(PhasedGenotypeType.Het, 0) + 1)
+        }
+      }
+      //Missing data is accumulated -- inconsistent with other genotypes but might be helpful to have..
+      if(byGT.contains(GenotypeType.NoCall)){
+        res.update(PhasedGenotypeType.HomVar, res.getOrElse(PhasedGenotypeType.HomVar, 0) + byGT(GenotypeType.NoCall).size)
+      }
+
+    })
+
+    res
+  }
+
+
+}
+
---
+++ src/main/scala/org/broadinstitute/hail/utils/SparseVariantSampleMatrix.scala
@@ -0,0 +1,105 @@
+package org.broadinstitute.hail.utils
+
+import org.broadinstitute.hail.variant.{Genotype, GenotypeType}
+import org.broadinstitute.hail.variant.GenotypeType._
+
+import scala.collection.mutable
+
+/**
+  * Created by laurent on 4/19/16.
+  */
+class SparseVariantSampleMatrix(val sampleIDs: IndexedSeq[String]) extends Serializable {
+
+  val nSamples = sampleIDs.length
+  private var variantIDs = IndexedSeq[String]()
+  //var variantSampleIndex = 0
+
+  val variants = mutable.Map[String, mutable.Map[String,Genotype]]()
+  val samples = mutable.Map[String, mutable.Map[String, Genotype]]()
+
+  def merge(that: SparseVariantSampleMatrix): SparseVariantSampleMatrix = {
+
+    variants ++= that.variants
+
+    that.samples foreach {case(s,variants) => {
+      if(samples.contains(s)){
+        samples.get(s).get ++= variants
+      }else{
+        samples.update(s,variants)
+      }
+    }}
+    this
+  }
+
+  def addVariantGenotype(variantID: String, sampleID: String, g: Genotype): SparseVariantSampleMatrix = {
+
+    if(!g.isHomRef) {
+      //Add genotype in variants
+      if (variants.contains(variantID)) {
+        variants.get(variantID).get.update(sampleID, Genotype(g.gt))
+      }else{
+        variants.update(variantID,mutable.HashMap(sampleID -> Genotype(g.gt)))
+      }
+
+      //Add variant in sample
+      if(samples.contains(sampleID)){
+        samples.get(sampleID).get.update(variantID,Genotype(g.gt))
+      }else{
+        samples.update(sampleID,mutable.HashMap(variantID -> Genotype(g.gt)))
+      }
+
+    }
+
+    //variants = variants.updated(v, mv)
+    this
+  }
+
+  def getVariant(variantID: String): Option[Map[String,Genotype]] = {
+    if(variants.contains(variantID)) {
+      Some(variants.get(variantID).get.toMap)
+    }
+    None
+  }
+
+  def getSample(sampleID: String): Option[Map[String,Genotype]] = {
+    if(sampleIDs.contains(sampleID)) {
+      if(samples.contains(sampleID)) {
+        Some(samples.get(sampleID).get.toMap)
+      }else{
+        Some(Map[String,Genotype]())
+      }
+    }
+    None
+  }
+
+  def getGenotype(variantID: String, sampleID:String) : Option[Genotype] = {
+    if (variants.contains(variantID) && sampleIDs.contains(sampleID)) {
+      variants.get(variantID).get.get(sampleID) match {
+        case (Some(g)) => Some(g)
+        case None => Some(Genotype(0)) //TODO find a way of not hardcoding this
+      }
+    }
+    None
+  }
+
+
+  def cumulativeAF: Double = {
+
+    variants.aggregate(0.0)({(acc, variant) =>
+      //Count the number of called samples and the number of non-ref alleles
+      val counts = variant._2.foldLeft((0.0,0.0))({(acc2,g) =>
+        g match {
+          case GenotypeType.NoCall => (acc2._1, acc2._2 + 1)
+          case GenotypeType.Het => (acc2._1 + 1, acc2._2)
+          case GenotypeType.HomVar => (acc2._1 + 2, acc2._2)
+          case GenotypeType.HomRef => acc2 //This is only here for completeness sake and should never be used
+        }
+      })
+      counts._1/(nSamples - counts._2)
+    },
+      {(acc1,acc2) => (acc1 + acc2)
+    })
+
+  }
+
+}
