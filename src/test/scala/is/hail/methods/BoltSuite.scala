be6841ccb961a41f11df9eb15f9c24bd7e9d3b09
Tom White <tom@cloudera.com>
2017-06-19 15:51:54 +0100

    Add a placeholder test

---
+++ src/test/scala/is/hail/methods/BoltSuite.scala
@@ -0,0 +1,12 @@
+package is.hail.methods
+
+import is.hail.SparkSuite
+import org.testng.annotations.Test
+
+class BoltSuite extends SparkSuite {
+
+  @Test def test(): Unit = {
+    println("hello")
+  }
+
+}
