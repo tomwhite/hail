efcb7f00e726285380b6ec1270bdf992b8f2cb7d
Tom White <tom@cloudera.com>
2017-06-26 10:48:19 +0100

    Factor out estimateVarianceParameters method

--- src/test/scala/is/hail/methods/BoltSuite.scala
+++ src/test/scala/is/hail/methods/BoltSuite.scala
@@ -5,35 +5,76 @@ import breeze.optimize.linear.ConjugateGradient
 import breeze.stats.distributions.Gaussian
 import is.hail.SparkSuite
 import org.testng.annotations.Test
+import scala.util.control.Breaks._
 
 class BoltSuite extends SparkSuite {
 
   @Test def test(): Unit = {
 
-    // Step 1a: Estimate variance parameters.
-    // p33 of supplement
     val X = DenseMatrix(
       (1.0, 0.1, 0.5, 0.2, 0.4),
       (0.3, 0.4, 0.1, 0.1, 0.7),
       (0.9, 0.3, 0.1, 0.8, 0.4))
-    val y = DenseVector(0.1, -0.4, 0.5)
-    val M = X.cols // 5
-    val N = X.rows // 3
+    val y = DenseVector(0.1, 0.4, 0.5)
+
+    val (sigmaGSq, sigmaESq) = estimateVarianceParameters(X, y)
+
+    println("Final results")
+    println("sigmaGSq: " + sigmaGSq)
+    println("sigmaESq: " + sigmaESq)
+  }
+
+  def estimateVarianceParameters(X: DenseMatrix[Double], y: DenseVector[Double]): (Double, Double) = {
+
+    // Step 1a: Estimate variance parameters.
+    // p33 of supplement
+
+    val M = X.cols
+    val N = X.rows
+    // TODO: check that y has N rows
+
     val mcTrials = math.max(math.min(4e9 / (N * N), 15), 3).toInt
     val betaRand = DenseMatrix.rand(M, mcTrials, Gaussian(0, math.sqrt(1.0 / M)))
     val eRandUnscaled = DenseMatrix.rand(N, mcTrials, Gaussian(0, 1))
 
-    val h1Sq = 0.25
-    val logDelta1 = math.log((1 - h1Sq) / h1Sq)
-    val (f1, sigmaGSq1, sigmaESq1) = evalfREML(logDelta1, X, y, mcTrials, betaRand, eRandUnscaled)
+    val maxS = 4 // TODO: 7
+    // use 1-indexes to match pseudocode
+    val hSq = new Array[Double](maxS + 1)
+    val logDelta = new Array[Double](maxS + 1)
+    val f = new Array[Double](maxS + 1)
+    var sigmaGSq = 0.0
+    var sigmaESq = 0.0
+    hSq(1) = 0.25
+    logDelta(1) = math.log((1 - hSq(1)) / hSq(1))
+    val t1 = evalfREML(logDelta(1), X, y, mcTrials, betaRand, eRandUnscaled)
+    f(1) = t1._1
+    sigmaGSq = t1._2
+    sigmaESq = t1._3
 
-    val h2Sq = if (f1 < 0) 0.125 else 0.5
-    val logDelta2 = math.log((1 - h2Sq) / h2Sq)
-    val (f2, sigmaGSq2, sigmaESq2) = evalfREML(logDelta2, X, y, mcTrials, betaRand, eRandUnscaled)
+    hSq(2) = if (f(1) < 0) 0.125 else 0.5
+    logDelta(2) = math.log((1 - hSq(2)) / hSq(2))
+    val t2 = evalfREML(logDelta(2), X, y, mcTrials, betaRand, eRandUnscaled)
+    f(2) = t2._1
+    sigmaGSq = t2._2
+    sigmaESq = t2._3
 
+    breakable {
+      for (s <- 3 until maxS) {
+        logDelta(s) = (logDelta(s - 2) * f(s - 1) - logDelta(s - 1) * f(s - 2)) / (f(s - 1) - f(s - 2))
+        if (math.abs(logDelta(s) - logDelta(s - 1)) < 0.01) {
+          break
+        }
+        val ts = evalfREML(logDelta(s), X, y, mcTrials, betaRand, eRandUnscaled)
+        f(s) = ts._1
+        sigmaGSq = ts._2
+        sigmaESq = ts._3
+      }
+    }
+
+    (sigmaGSq, sigmaESq)
   }
 
-  def evalfREML(logDelta: Double, X: DenseMatrix[Double], y: DenseVector[Double], mcTrials: Int, betaRand: DenseMatrix[Double], eRandUnscaled: DenseMatrix[Double]): Tuple3[Double, Double, Double]
+  def evalfREML(logDelta: Double, X: DenseMatrix[Double], y: DenseVector[Double], mcTrials: Int, betaRand: DenseMatrix[Double], eRandUnscaled: DenseMatrix[Double]): (Double, Double, Double)
   = {
     val M = X.cols
     val N = X.rows
